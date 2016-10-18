0c610424e859f95e1a90fa24aaeffe164dc5929e
Spencer Tranter <spencertranter95@gmail.com>
2016-10-18 21:54:09 +0000

    Changed variable @product.review to @product.reviews as there are multiple

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,10 +19,6 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
-  def clear_cart
-    @cart = {}
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
